f5640751d1a2ea57dd52490161dcb7c8828dba76
Placido Wang <placidow@gmail.com>
2020-01-30 21:25:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,4 +12,14 @@
 # end
 
 def fizzbuzz(num)
+  output = ""
   
+  output += "Fizz" if num % 3 === 0
+  output += "Buzz" if num % 5 === 0
+  
+  if output === ""
+    nil
+  else
+    output
+  end
+end
